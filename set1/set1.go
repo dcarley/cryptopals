6563d2173417af80bd36e1285052de57bc8c09d2
Dan Carley <dan.carley@gmail.com>
2017-06-19 08:30:37 +0100

    s1c1: Skeleton and tests.

--- set1/set1.go
+++ set1/set1.go
@@ -1,3 +1,8 @@
 package set1
 
 import ()
+
+// HexToBase64 converts hexidecimal encoded text to base64.
+func HexToBase64(text []byte) ([]byte, error) {
+	return []byte(""), nil
+}
--- set1/set1_test.go
+++ set1/set1_test.go
@@ -8,5 +8,13 @@ import (
 )
 
 var _ = Describe("Set1", func() {
-
+	Describe("Challenge1", func() {
+		Describe("HexToBase64", func() {
+			It("should convert example", func() {
+				b64, err := HexToBase64([]byte("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
+				Expect(err).ToNot(HaveOccurred())
+				Expect(b64).To(Equal([]byte("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")))
+			})
+		})
+	})
 })
