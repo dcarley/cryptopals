fd116f9b4476f286c309b994ae5519f0b9eae2b4
Dan Carley <dan.carley@gmail.com>
2017-06-19 08:49:58 +0100

    s1c1: Implementation using standard library
    I'm not sure this is what was intended so I'm going to replace the standard
    library calls with my own code in subsequent commits.

--- set1/set1.go
+++ set1/set1.go
@@ -1,8 +1,20 @@
 package set1
 
-import ()
+import (
+	"encoding/base64"
+	"encoding/hex"
+)
 
 // HexToBase64 converts hexidecimal encoded text to base64.
 func HexToBase64(text []byte) ([]byte, error) {
-	return []byte(""), nil
+	raw := make([]byte, hex.DecodedLen(len(text)))
+	_, err := hex.Decode(raw, text)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	b64 := make([]byte, base64.StdEncoding.EncodedLen(len(raw)))
+	base64.StdEncoding.Encode(b64, raw)
+
+	return b64, nil
 }
