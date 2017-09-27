f0fc1a3e421578b1c6b8439796818459839c22cb
Dan Carley <dan.carley@gmail.com>
2017-09-27 22:52:27 +0100

    s1c5: Implementation
    I had to concat the two lines of hex encoded output.

--- set1/set1_test.go
+++ set1/set1_test.go
@@ -215,4 +215,16 @@ var _ = Describe("Set1", func() {
 			Expect(highestScore.Text).To(Equal([]byte("Now that the party is jumping\n")))
 		})
 	})
+
+	Describe("Challenge5", func() {
+		It("should solve example", func() {
+			output, err := RepeatingKeyXOR(
+				[]byte(`Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal`),
+				[]byte("ICE"),
+			)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(HexEncode(output)).To(Equal([]byte("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f")))
+		})
+	})
 })
